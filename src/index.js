feb51c47d4bb73269ccfae1d3dd76369b86c6568
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2022-04-20 10:51:52 +0530

    debug

--- src/index.js
+++ src/index.js
@@ -11,7 +11,12 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-console.log({ domain }, { clientId });
+console.log(
+  { domain },
+  { clientId },
+  process.env.REACT_APP_AUTH0_DOMAIN,
+  process.env.REACT_APP_AUTH0_CLIENT_ID
+);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
